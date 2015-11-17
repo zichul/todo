d5c075198ea6e7ef8f8bed9141dda1760e26cc33
zichul <zichul@gmail.com>
2015-11-17 15:53:08 +0100

    autorefresh subscribe

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,48 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+
+
+function runAutoRefresh(token, csfr) {
+  // Connect to Socky Server
+  var socky = new Socky.Client('ws://localhost:3001/websocket/todo');
+  
+  // Bind message after connecting to server
+  socky.bind("socky:connection:established", function() {
+    console.log("Connected");
+  });
+  
+  // Bind message after disconnecting from server
+  socky.bind("socky:connection:closed", function() {
+    console.log("Connection closed");
+  });
+
+  // Subscribe to channel
+  // You can subscribe to as much channels as you want
+  // { write:true } option will allow sending messages directly to server
+  //   note: this will require enabling this in authenticator
+  // { data: { login: username } } all 'data' options are passed to other users
+  var channel = socky.subscribe(token, { write: true });
+  
+  // Bind message after successfull joining channel
+  channel.bind("socky:subscribe:success", function(members) {
+    console.log("Joined channel");
+  });
+  
+  // Bind function to 'chat_message' event
+  // This event can be sent by all clients with 'write' permission
+  channel.bind("reload", function(message) {
+    if (message.token == token) {
+      location.reload();
+    }
+  });
+  
+  // jQuery bind sending message via form to channel event
+  $("#add_todo").submit(function(e) {
+    e.preventDefault();
+    channel.trigger("reload", { token: token });
+    $('#message').val('')
+    return false;
+  });
+};
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  post 'socky/auth' => 'sockies#auth'
 
-  resources :lists
   resources :todos, only: [:create, :update, :destroy]
   post 'toggle/:id' => 'todos#toggle', as: 'toggle_todo'
 
+  resources :lists
   get 'reset_token/:id' => 'lists#reset_token', as: 'reset_token'
   get 'shared/:token' => 'lists#shared', as: 'shared'
 
